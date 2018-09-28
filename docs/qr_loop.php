b9b28d24476f2d29c7217b45c3a498f5038fc978
dpohl <dpohl@q21.de>
2018-09-28 14:51:23 +0200

    smaller back to map button and new docs

--- docs/qr_loop.php
+++ docs/qr_loop.php
@@ -1,12 +1,80 @@
 <?php
-        //Enter your code here, enjoy!
-$count = 5;
-              
-for( $i = 1; $i <= $count; $i++ ){
-    if ($i !== $count) {
-        echo '{"exhibit": {"room":1,"exponat":'. $i .'}, "qrType": "section"};';
+// http://moodle.mathetics.local/theme/badges/qr_loop.php?mode=user&userPrefix=visitor&passwordPrefix=aldenBiesen&visitorStart=1000&visitorEnd=1002
+// http://moodle.mathetics.local/theme/badges/qr_loop.php?mode=topic&mapNumber=1&roomNumber=1&exhibitStart=1&exhibitEnd=10
+// http://moodle.mathetics.local/theme/badges/qr_loop.php?mode=topic&roomNumber=1&exhibitStart=1&exhibitEnd=10
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if (isset($_REQUEST['mode'])) {
+$mode = test_input($_REQUEST['mode']);
+}
+
+if (isset($_REQUEST['userPrefix'])) {
+$userPrefix = $_REQUEST['userPrefix'];
+}
+
+if (isset($_REQUEST['passwordPrefix'])) {
+$passwordPrefix = $_REQUEST['passwordPrefix'];
+}
+
+if (isset($_REQUEST['visitorStart'])) {
+$visitorStart = $_REQUEST['visitorStart'];
+}
+
+if (isset($_REQUEST['visitorEnd'])) {
+$visitorEnd = $_REQUEST['visitorEnd'];
+}
+
+if (isset($_REQUEST['mapNumber'])) {
+$mapNumber = $_REQUEST['mapNumber'];
+}
+
+if (isset($_REQUEST['roomNumber'])) {
+$roomNumber = $_REQUEST['roomNumber'];
+}
+
+if (isset($_REQUEST['exhibitStart'])) {
+$exhibitStart = $_REQUEST['exhibitStart'];
+}
+
+if (isset($_REQUEST['exhibitEnd'])) {
+$exhibitEnd = $_REQUEST['exhibitEnd'];
+}
+
+if ($mode === 'user') {
+    for( $i = $visitorStart; $i <= $visitorEnd; $i++ ){
+        if ($i != $visitorEnd) {
+            echo '{"username": "'. $userPrefix.$i. '", "password": "'. $passwordPrefix.$i. '!", "qrType": "login"};';
+        } else {
+            echo '{"username": "'. $userPrefix.$i. '", "password": "'. $passwordPrefix.$i. '!", "qrType": "login"}';
+        }
+        
+    }
+}
+
+if ($mode === 'topic') {
+    if (isset($mapNumber)) {
+        for( $i = $exhibitStart; $i <= $exhibitEnd; $i++ ){
+            if ($i != $exhibitEnd) {
+                echo '{"exhibit": {"map":'.$mapNumber .',"room":'.$roomNumber .',"exponat":'.$i .'}, "qrType": "section"};';
+            } else {
+                echo '{"exhibit": {"map":'.$mapNumber .',"room":'.$roomNumber .',"exponat":'.$i .'}, "qrType": "section"}';
+            }
+            
+        }
     } else {
-        echo '{"exhibit": {"room":1,"exponat":'. $i .'}, "qrType": "section"}';
+        for( $i = $exhibitStart; $i <= $exhibitEnd; $i++ ){
+            if ($i != $exhibitEnd) {
+                echo '{"exhibit": {"room":'.$roomNumber .',"exponat":'.$i .'}, "qrType": "section"};';
+            } else {
+                echo '{"exhibit": {"room":'.$roomNumber .',"exponat":'.$i .'}, "qrType": "section"}';
+            }
+            
+        }
     }
     
 }
